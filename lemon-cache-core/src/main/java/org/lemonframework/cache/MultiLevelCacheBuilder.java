28ca197be69499125b85824b2a6a8bf3e7698168
sanye <909861882@qq.com>
2018-12-17 14:57:06 +0800

    增加multi builder

---
+++ lemon-cache-core/src/main/java/org/lemonframework/cache/MultiLevelCacheBuilder.java
@@ -0,0 +1,73 @@
+package org.lemonframework.cache;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+/**
+ * builder.
+ *
+ * @author jiawei zhang
+ * @since 0.0.1
+ */
+public class MultiLevelCacheBuilder<T extends MultiLevelCacheBuilder<T>> extends AbstractCacheBuilder<T> {
+    public static class MultiLevelCacheBuilderImpl extends MultiLevelCacheBuilder<MultiLevelCacheBuilderImpl> {
+    }
+
+    public static MultiLevelCacheBuilderImpl createMultiLevelCacheBuilder() {
+        return new MultiLevelCacheBuilderImpl();
+    }
+
+    protected MultiLevelCacheBuilder() {
+        buildFunc(config -> new MultiLevelCache((MultiLevelCacheConfig) config));
+    }
+
+    @Override
+    public MultiLevelCacheConfig getConfig() {
+        if (config == null) {
+            config = new MultiLevelCacheConfig();
+        }
+        return (MultiLevelCacheConfig) config;
+    }
+
+    public T addCache(Cache... caches) {
+        for (Cache c : caches) {
+            getConfig().getCaches().add(c);
+        }
+        return self();
+    }
+
+    public void setCaches(List<Cache> caches) {
+        getConfig().setCaches(caches);
+    }
+
+    public T useExpireOfSubCache(boolean useExpireOfSubCache) {
+        getConfig().setUseExpireOfSubCache(useExpireOfSubCache);
+        return self();
+    }
+
+    public void setUseExpireOfSubCache(boolean useExpireOfSubCache) {
+        getConfig().setUseExpireOfSubCache(useExpireOfSubCache);
+    }
+
+    @Override
+    public T keyConvertor(Function<Object, Object> keyConvertor) {
+        throw new UnsupportedOperationException("MultiLevelCache do not need a key convertor");
+    }
+
+    @Override
+    public void setKeyConvertor(Function<Object, Object> keyConvertor) {
+        throw new UnsupportedOperationException("MultiLevelCache do not need a key convertor");
+    }
+
+    @Override
+    public T expireAfterAccess(long defaultExpire, TimeUnit timeUnit) {
+        throw new UnsupportedOperationException("MultiLevelCache do not support expireAfterAccess");
+    }
+
+    @Override
+    public void setExpireAfterAccessInMillis(long expireAfterAccessInMillis) {
+        throw new UnsupportedOperationException("MultiLevelCache do not support expireAfterAccess");
+    }
+
+}
